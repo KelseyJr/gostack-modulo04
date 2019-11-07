e2c52fbed2f5833a88c81d1519f4fd4fefba1a35
Kelsey Junior <kelseytfjunior@gmail.com>
2019-11-06 21:59:19 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // interpretador js
+    '@babel/preset-react', // interpretador react
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties'
+  ]
+}
